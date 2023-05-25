65287eeab93a32a438a52c443569cf70448a1223
ariefdwisantoso <arief.dwisantoso@gmail.com>
2023-05-25 15:49:00 +0700

    setting swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,27 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-// class Controller extends BaseController
-// {
-//     use AuthorizesRequests, ValidatesRequests;
-// }
-
-/**
- * @OA\Info(
- *    title="Your super  ApplicationAPI",
- *    version="1.0.0",
- * )
- * @OA\Get(
- *    path="/",
- *    description="Home page",
- *    @OA\Response(response="default", description="welcome")
- * )
- */
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
